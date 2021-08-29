00e5d05c948b367843ae18167a936449b5dfb9a3
Ezegrandia <ezequielgrandia@gmail.com>
2021-08-29 15:56:18 -0300

    Conecto las imagenes con los items segun si id, y ordeno el orden de los proyectos con un parametro en el fetch de la api

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function main() {
   //hago el fetch a la API de contentful y vinculo la ultima response con la funct dataProcess(apiData)
   fetch(
-    "https://cdn.contentful.com/spaces/9eckq975r12y/environments/master/entries?access_token=moNXdOHXyYw91xx7JXsj766e4MeK1k8lpX4-0S-JWa0&content_type=work"
+    "https://cdn.contentful.com/spaces/9eckq975r12y/environments/master/entries?access_token=moNXdOHXyYw91xx7JXsj766e4MeK1k8lpX4-0S-JWa0&content_type=work&order=sys.createdAt"
   )
     .then((response) => response.json())
     .then((data) => dataProcess(data));
@@ -11,19 +11,37 @@ function dataProcess(apiData) {
   //recibo la data en json de la API y genero el objeto con los parametros
   const itemsArray = apiData.items;
   const imgsArray = apiData.includes.Asset;
+
   console.log(apiData, "soy apiData");
+
+  //creo el objeto para ir iterando y obteniendo los params a pasar
   const objParams = {
     title: "",
     description: "",
     img: "",
     url: "",
   };
+
+  // Creo un objeto para ir iterando y obteniendo la id de la imagen correcta
+  const itemImgId = {
+    item: 0,
+    id: "",
+  };
+
   //por cada item del array remplazo la info del objeto con la de la API y se lo mando a la funct addWorkCard(params)
-  itemsArray.forEach((item) => {
+  itemsArray.forEach((item, i) => {
     objParams.title = item.fields.titulo;
     objParams.description = item.fields.descripcion;
-    objParams.img = imgsArray[0].fields.file.url;
     objParams.url = item.fields.url;
+
+    itemImgId.item = i;
+    itemImgId.id = item.fields.imagen.sys.id;
+
+    imgsArray.forEach((img) => {
+      if (itemImgId.id == img.sys.id) {
+        objParams.img = img.fields.file.url;
+      }
+    });
 
     addWorkCard(objParams);
   });
